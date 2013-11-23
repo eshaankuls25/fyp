ac625d5584fb4a2964b6d0535c422134366f78d0
Luther P <post2base@outlook.com>
2013-11-23 12:52:16 +0000

    Fixed docker build script.

--- set/buildSandbox.sh
+++ set/buildSandbox.sh
@@ -4,8 +4,8 @@
 #
 
 git clone https://github.com/Quantza/fyp ~/fyp/
-
-cp ~/fyp/set/code/Dockerfiles/Dockerfile ~
+mkdir ~/docker_build/ && cd ~/docker_build/
+cp ~/fyp/set/code/Dockerfiles/Dockerfile .
 sudo docker build -t set_box .
 
 
