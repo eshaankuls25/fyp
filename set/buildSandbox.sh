b02668e8bc563f304baa26206ef13aabb81ad3d4
Luther P <post2base@outlook.com>
2013-11-23 13:18:56 +0000

    Updated buildSandbox.sh

--- set/buildSandbox.sh
+++ set/buildSandbox.sh
@@ -3,9 +3,23 @@
 # Script to start docker container build
 #
 
-git clone https://github.com/Quantza/fyp ~/fyp/
-mkdir ~/docker_build/ && cd ~/docker_build/
-cp ~/fyp/set/code/Dockerfiles/Dockerfile .
+DOCKER_BUILD_PATH=~/docker_build/
+DOCKERFILE=~/fyp/set/code/Dockerfiles/Dockerfile
+GIT_PROJECT_PATH=~/fyp/
+
+if [ -d "$GIT_PROJECT_PATH" ]; then
+rm -rf $GIT_PROJECT_PATH
+fi
+
+if [ ! -d "$DOCKER_BUILD_PATH" ]; then
+mkdir $DOCKER_BUILD_PATH
+fi
+
+git clone https://github.com/Quantza/fyp $GIT_PROJECT_PATH
+
+cd $DOCKER_BUILD_PATH
+cp $DOCKERFILE .
+
 sudo docker build -t set_box .
 
 
