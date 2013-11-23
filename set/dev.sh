bf0e5fe6b262db852c81bcf7c9c17dce33874347
Luther P <post2base@outlook.com>
2013-11-23 12:01:36 +0000

    Updated dev.sh

--- set/dev.sh
+++ set/dev.sh
@@ -12,5 +12,11 @@ if [ "$option" = 'y' ]; then
 sudo cp ~/set/code/Python/*.py /usr/share/setoolkit
 fi
 
+echo "To perform packet capture, press 'y'. Otherwise press another key.";
+read option
+
+if [ "$option" = 'y' ]; then
 gnome-terminal -e 'sudo python /usr/share/setoolkit/packetcapture.py'
+fi
+
 sudo python /usr/share/setoolkit/startTool.py
