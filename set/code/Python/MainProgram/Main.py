3360807c006396cc5048af8e8b87cb549134f171
Luther P <post2base@outlook.com>
2013-12-22 17:55:47 +0000

    Added Main.py

--- set/code/Python/MainProgram/Main.py
+++ set/code/Python/MainProgram/Main.py
@@ -3,22 +3,26 @@ import ParserSelector
 import TextParser
 import FeatureSet
 
+import sys
+import getopt
+
 def main():
 
 	#Allow user to pass document types, document name/file,
 	#and parser identifier set(s) in as parameters.
 
-	#documentTypes = None
-	documentTypes = initialiseDocumentTypes(['fake', 'list', 'of', 'document', 'types', 'example:', 'html', 'text'])
-
-
+	options, extras = getopt.getopt(sys.argv[1:], 'd:t:', ['docname=',
+															'type='])
+	
+	for opt, arg in options:
+    if opt in ('-d', '--docname'):
+        documentName = arg
+    elif opt in ('-t', '--type'):
+        documentType = arg
 
 	preprocessor = PreProcessor()
-	ps = ParserSelector()
+	ps = ParserSelector('fake', 'list', 'of', 'document', 'categories', 'example:', 'html', 'text')
 	textParser = TextParser()
-
-def initialiseDocumentTypes(*sequential, **named):	#From StackOverflow - http://stackoverflow.com/questions/36932/how-can-i-represent-an-enum-in-python
-    	return dict(zip(sequential, range(len(sequential))), **named)
 
 
 if __name__ == "__main__":
