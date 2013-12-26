b528dabfff6bcb7fae0032d2f9b958cf3dbe72e6
Quantza <post2base@outlook.com>
2013-12-26 00:32:44 +0000

    Added scrapy code

---
+++ set/code/Python/MainProgram/setWebScan/setWebScan/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for setWebScan project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'setWebScan'
+
+SPIDER_MODULES = ['setWebScan.spiders']
+NEWSPIDER_MODULE = 'setWebScan.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'setWebScan (+http://www.yourdomain.com)'
---
+++ set/code/Python/MainProgram/setWebScan/setWebScan/spiders/set_spider.py
@@ -0,0 +1,13 @@
+from scrapy.spider import BaseSpider
+
+class SETSpider(BaseSpider):
+    name = "set"
+    allowed_domains = ["localhost"]
+
+    start_urls = [
+        "http://127.0.0.1/"
+        ]
+
+    def parse(self, response):
+        filename = response.url.split("/")[-2]
+        open(filename, 'wb').write(response.body)
