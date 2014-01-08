02990f3ccfcb815054398fa0f887911499a7a65f
Quantza <post2base@outlook.com>
2014-01-08 00:44:09 +0000

    Fixed code

--- set/code/Python/MainProgram/Main.py
+++ set/code/Python/MainProgram/Main.py
@@ -27,7 +27,7 @@ def main():
 
         categoryList = ['text', 'html']
 
-        extractorList = [(ImitationFeatureExtractor(), ObfuscationFeatureExtractor()), HTMLFeatureExtractor(False)]
+        extractorList = [(ImitationFeatureExtractor(), ObfuscationFeatureExtractor()), HTMLFeatureExtractor(True)]
     
         indicatorDictionary = {'text':['From:', 'Date:', 'Message-ID', 'In-Reply-To:'],\
                               'html':['http://', 'www', '.com', '.co.uk']}
